7a11d0eb92d9c42608edbc57e1729ec45761d910
unknown <evans95mm@gmail.com>
2020-03-30 12:58:46 +0200

    Menu Overlay

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@ const menuBtn=document.querySelector('.menu-btn');
 const menu=document.querySelector('.menu');
 const menuNav=document.querySelector('.menu-nav');
 const menuBranding=document.querySelector('.menu-branding');
-
 const navItems=document.querySelectorAll('.nav-item');
 
 //set initial state of menu
 let showMenu=false;
 
-menuBtn.addEventListener('click',toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
     if(!showMenu){
@@ -28,6 +27,7 @@ function toggleMenu(){
        menuNav.classList.remove('show');
        menuBranding.classList.remove('show');
        navItems.forEach(item =>item.classList.add('show'));
+       showMenu=false;
     }
-    showMenu=true;
+  
 }
